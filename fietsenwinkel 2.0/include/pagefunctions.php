2888b79044f065e2606e43c36641ffc6cc31a792
Haben13 <138872@student.horizoncollege.nl>
2022-02-17 13:34:42 +0100

    In layout.php wordt de pagina opgebouwd

---
+++ fietsenwinkel 2.0/include/pagefunctions.php
@@ -0,0 +1,28 @@
+<?php
+function getTitle(){
+    return "Fietsenwinkel";
+}
+
+function getHeader(){
+    return "Fietsenwinkel  2.0";
+}
+
+function getNav(){
+    $nav = "
+  <ul>
+  <li>Home</li>
+  <li>Item 1</li>
+  <li>Item 2</li>
+  </ul>
+";
+return $nav; 
+
+}
+
+function getFooter(){
+    return "Copyright 2021 - Haben Sebhatu";
+}
+
+function getAside(){
+    return "aside tekst";
+}
