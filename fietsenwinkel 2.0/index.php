11b42415c78dfc94257413929ddbc855900a303a
Haben13 <138872@student.horizoncollege.nl>
2022-02-17 13:17:50 +0100

    Code gekopieerd van vorige opdract

---
+++ fietsenwinkel 2.0/index.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+
+
+
+require_once ("include/dbfunctions.php");
+
+require_once("include/formfunctions.php");
+
+require_once("include/pagefunctions.php");
+
+require_once("include/userfunctions.php");
+
+require_once("include/fietsfunctions.php");
+
+
+
+$content = "Dit is de content";
+
+require ("include/layout.php");
